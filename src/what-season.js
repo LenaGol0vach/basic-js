788880b2c70468bb282f76326bc08da6e53b1dfb
LenaGol0vach <leshapantin@icloud.com>
2021-03-01 13:09:23 +0300

    init: finish what season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!";
+  if (!date.getTime()) throw new Error();
+  let month = date.getMonth();
+  if (month == 11 || month == 0 || month == 1) {
+    return "winter";
+  } else if (month < 5) {
+    return "spring";
+  } else if (month < 8) {
+    return "summer";
+  } else return "autumn";
 };
