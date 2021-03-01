4383990571a4e672bb4e1b14475d377caca8fd4e
LenaGol0vach <leshapantin@icloud.com>
2021-03-01 17:43:43 +0300

    init: finish transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  let massiv = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "--discard-next") {
+      i++;
+    } else if (arr[i] == "--discard-prev") {
+      if (massiv.length !== 0 && arr[i - 2] !== "--discard-next") {
+        massiv.pop();
+      }
+    } else if (arr[i] == "--double-next") {
+      if (i < arr.length - 1) {
+        massiv.push(arr[i + 1]);
+      }
+    } else if (arr[i] == "--double-prev") {
+      if (i !== 0 && arr[i - 2] !== "--discard-next") {
+        massiv.push(arr[i - 1]);
+      }
+    } else massiv.push(arr[i]);
+  }
+  return massiv;
 };
